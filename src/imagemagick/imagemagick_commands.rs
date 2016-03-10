539a182c8e3194dc537f1ad465a18ce060b5da86
LambdaXymox <lambda.xymox@gmail.com>
2016-03-10 02:28:33 -0600

    Remove unused imports.

--- src/imagemagick/imagemagick_commands.rs
+++ src/imagemagick/imagemagick_commands.rs
@@ -2,9 +2,7 @@
 use std::process::{Command, Output};
 use std::string::{String, ToString};
 use std::ffi::OsString;
-use std::path::Path;
-use std::io::{Result, Error};
-use std::result;
+use std::io;
 
 
 pub type FilePath = String;
@@ -34,19 +32,19 @@ impl ToString for ImageMagickCommand {
 fn imagemagick_command(command_name: ImageMagickCommand, 
                        file_path: &FilePath, 
                        args: &[String]) 
-    -> Result<String> {
+    -> io::Result<String> {
 
     let mut command_args: Vec<OsString> = Vec::new();
     for arg in args {
         command_args.push(OsString::from(arg));
     }
 
-    let output: Result<Output> = Command::new(command_name.to_string())
+    let output: io::Result<Output> = Command::new(command_name.to_string())
                                         .args(command_args.as_ref())
                                         .arg(file_path)
                                         .output();
     
-    let result: Result<String> = match output {
+    let result: io::Result<String> = match output {
         Ok(output) => Ok(String::from_utf8(output.stdout).unwrap()),
         Err(e)     => Err(e),
     };
@@ -55,32 +53,32 @@ fn imagemagick_command(command_name: ImageMagickCommand,
 }
 
 #[inline]
-fn imagemagick_identify(file_path: &FilePath, args: &[String]) -> Result<String> {
+fn imagemagick_identify(file_path: &FilePath, args: &[String]) -> io::Result<String> {
     imagemagick_command(ImageMagickCommand::Identify, file_path, args)
 }
 
 
-pub fn imagemagick_identify_default(file_path: &FilePath) -> Result<String> {
+pub fn imagemagick_identify_default(file_path: &FilePath) -> io::Result<String> {
     imagemagick_identify(file_path, &[])
 }
 
 
-pub fn imagemagick_identify_verbose(file_path: &FilePath) -> Result<String> {
+pub fn imagemagick_identify_verbose(file_path: &FilePath) -> io::Result<String> {
     let args = ["-verbose".to_string()];
 
     imagemagick_identify(file_path, &args)
 }
 
 
-pub fn imagemagick_mogrify(file_path: &FilePath, args: &[String]) -> Result<String> {
+pub fn imagemagick_mogrify(file_path: &FilePath, args: &[String]) -> io::Result<String> {
     imagemagick_command(ImageMagickCommand::Mogrify, file_path, args)
 }
 
 
-pub fn imagemagick_convert(file_path: &FilePath, args: &[String]) -> Result<String> {
+pub fn imagemagick_convert(file_path: &FilePath, args: &[String]) -> io::Result<String> {
     imagemagick_command(ImageMagickCommand::Convert, file_path, args)
 }
 
-pub fn imagemagick_no_operation() -> Result<String> {
+pub fn imagemagick_no_operation() -> io::Result<String> {
     Ok(String::from("No Operation"))
 }
